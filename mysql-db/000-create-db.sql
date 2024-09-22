e517586780a77ea00441b2d3cdd6a4238d9494e4
dmise123 <www.artdraw@gmail.com>
2024-09-22 14:50:09 +0700

    database-new

--- mysql-db/000-create-db.sql
+++ mysql-db/000-create-db.sql
@@ -1,1 +1,15 @@
-CREATE DATABASE manpro;
+-- Create the database
+CREATE DATABASE IF NOT EXISTS manpro;
+
+-- Switch to the new database
+USE manpro;
+
+-- Create the 'user' table
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(255) NOT NULL UNIQUE,
+  `username` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `join_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
