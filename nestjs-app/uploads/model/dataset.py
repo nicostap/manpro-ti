cd1aee27c32f9a99bf0147a067bf50379ba6e6f6
nicostap <nicoalexandrekurniawan@gmail.com>
2024-09-23 23:43:03 +0700

    Updated python scripts

--- nestjs-app/uploads/model/dataset.py
+++ nestjs-app/uploads/model/dataset.py
@@ -7,7 +7,7 @@ class ImageDataset(Dataset):
     def __init__(self, root_image, transform=None):
         self.root_image = root_image
         self.transform = transform
-        self.source_image_name = "source.jpg"
+        self.source_image_name = "source.png"
 
         self.source_image_path = os.path.join(self.root_image, self.source_image_name)
         if not os.path.exists(self.source_image_path):
@@ -15,7 +15,7 @@ class ImageDataset(Dataset):
         self.length_dataset = 1
 
     def __len__(self):
-        return self.length_dataset
+        return 1
 
     def __getitem__(self, index):
         image_img = np.array(Image.open(self.source_image_path).convert("RGB"))
--- nestjs-app/uploads/model/execute.py
+++ nestjs-app/uploads/model/execute.py
@@ -1,21 +1,22 @@
 import torch
 from dataset import ImageDataset
 import sys
-from utils import save_checkpoint, load_checkpoint
+from utils import load_checkpoint
 from torch.utils.data import DataLoader
 import torch.nn as nn
 import torch.optim as optim
 import config
-from tqdm import tqdm
 from torchvision.utils import save_image
-from discriminator_model import Discriminator
 from generator_model import Generator
 import sys
+from super_image import DrlnModel, ImageLoader
+from PIL import Image
 
 
-def execute(dataset, gen_H):
-    image = dataset[0]
-
+def execute(loader, gen_H):
+    image = next(iter(loader))
+    image = image.to(config.DEVICE)
+    use_amp = torch.cuda.is_available()
     with torch.cuda.amp.autocast() if use_amp else torch.enable_grad():
         fake_painting = gen_H(image)
     save_image(fake_painting * 0.5 + 0.5, f"{sys.argv[1]}/result.png")
@@ -23,20 +24,16 @@ def execute(dataset, gen_H):
 
 def main():
     gen_H = Generator(img_channels=3, num_residuals=9).to(config.DEVICE)
+    load_checkpoint(config.CHECKPOINT_GEN_H,gen_H)
+    dataset = ImageDataset(root_image=sys.argv[1],transform=config.transforms)
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
+    execute(dataloader, gen_H)
     
-    load_checkpoint(
-        config.CHECKPOINT_GEN_H,
-        gen_H,
-        opt_gen,
-        config.LEARNING_RATE,
-    )
-
-    dataset = ImageDataset(
-        root_image=sys.argv[1],
-        transform=config.transforms,
-    )
-
-    execute(dataset, gen_H)
+    image = Image.open(f"{sys.argv[1]}/result.png")
+    model = DrlnModel.from_pretrained('eugenesiow/drln-bam', scale=2)
+    inputs = ImageLoader.load_image(image)
+    preds = model(inputs)
+    ImageLoader.save_image(preds, f"{sys.argv[1]}/result.png")
 
 if __name__ == "__main__":
     main()
