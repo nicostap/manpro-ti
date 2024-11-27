f3515283205ca998669644b3f68381877f7cc222
dmise123 <www.artdraw@gmail.com>
2024-11-27 16:49:26 +0700

    tidy

--- vite-app/public/js/blurBall.js
+++ vite-app/public/js/blurBall.js
@@ -7,29 +7,27 @@ const screenHeight = window.innerHeight;
 // Initial position of the ball
 let posX = (screenWidth - ballSize*3);
 let posY = (screenHeight - ballSize);
-let directionX = Math.random() < 0.5 ? 1 : -1; // Random initial direction (left or right)
-let directionY = Math.random() < 0.5 ? 1 : -1; // Random initial direction (up or down)
+let directionX = Math.random() < 0.5 ? 1 : -1; 
+let directionY = Math.random() < 0.5 ? 1 : -1; 
 
-// Function to update ball position
+
 function moveBall() {
-  posX += directionX * 2; // Adjust speed here
+  posX += directionX * 2; 
   posY += directionY * 2;
 
-  // Check for boundaries and reverse direction if needed
   if (posX + ballSize> screenWidth-ballSize || posX < -ballSize*2) {
-    directionX *= -1; // Reverse X direction
+    directionX *= -1; 
   }
   if (posY + ballSize > screenHeight || posY < -ballSize) {
-    directionY *= -1; // Reverse Y direction
+    directionY *= -1; 
   }
 
-  // Apply the position to the ball
+  
   ball.style.transform = `translate(${posX}px, ${posY}px)`;
 
-  // Repeat the movement
   requestAnimationFrame(moveBall);
 }
 
-// Start moving the ball
+
 moveBall();
 
